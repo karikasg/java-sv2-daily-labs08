e5e66c41a807f9dd6ba4a00f0580bdd06b42f80c
karikasg <karikas.gabor@gmail.com>
2021-11-29 15:51:42 +0100

    8. hét 1. nap feladat (NumberSequence)

---
+++ src/main/java/NumberSequenceMain.java
@@ -0,0 +1,13 @@
+import day01.NumberSequence;
+
+import java.util.Arrays;
+
+public class NumberSequenceMain {
+    public static void main(String[] args) {
+        NumberSequence numberSequence = new NumberSequence(100, 1, 20);
+        System.out.println(numberSequence);
+
+        NumberSequence numberSequence2 = new NumberSequence(Arrays.asList(4, 6, 2, 7, 5, 3, 12, 9, 20));
+        System.out.println(numberSequence2);
+    }
+}
---
+++ src/main/java/day01/NumberSequence.java
@@ -0,0 +1,33 @@
+package day01;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+//Készíts egy day01.NumberSequence osztályt, aminek van egy egész számokból álló listája.
+// Vagy egy kész listát kap konstruktoron keresztül és ez lesz az attribútum értéke,
+// vagy három számot kap, melyből az első hogy hány véletlen számot generáljon a másik kettő pedig a minimum és maximum érték ami között generáljon.
+// Legyen egy metódus closeToAverage(int value) ,ami kigyűjti és visszaadja azokat a számokat egy listában,
+// amik az átlagtól legfeljebb a paraméterül átadott értékben térnek el!
+
+public class NumberSequence {
+    private List<Integer> numberList = new ArrayList<>();
+
+    public NumberSequence(List<Integer> numberList) {
+        this.numberList = numberList;
+    }
+
+    public NumberSequence(int piece, int min, int max) {
+        Random random = new Random();
+        for (int i = 0; i < piece; i++) {
+            numberList.add(random.nextInt(max - min+1) + min);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "NumberSequence{" +
+                "numberList=" + numberList +
+                '}';
+    }
+}
